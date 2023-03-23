12b9228a37251751294d240ec949bb2884ed9885
ElizabethMbulwa <elizabeth.mutuku21@students.dkut.ac.ke>
2023-03-23 03:00:08 -0700

    using input

---
+++ basic-math.py
@@ -0,0 +1,15 @@
+print(8+4)
+
+a=5
+b=10
+print(a+b)
+
+c=6.6
+d=2.1
+print(c+d)
+
+a= float (input("enter a number:"))
+b= float (input("enter another number:"))
+sum =a+b
+print ("the sum =  %d" %sum)
+    
