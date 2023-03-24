240084e3b410f9a87d7e9b4c63fc6fcd6b668c8b
ElizabethMbulwa <elizabeth.mutuku21@students.dkut.ac.ke>
2023-03-24 00:37:45 -0700

    using input

--- basic-math.py
+++ basic-math.py
@@ -1,15 +1,31 @@
-print(8+4)
+print(" ---------------Basic calc--------------")
 
-a=5
-b=10
-print(a+b)
+while True:
+    function= int(input('''
+    press 1 for addition
+    press 2 for subtraction
+    press 3 for  division
+    press 4 for  multiplication
+    press 0 to quit
+    '''))
+    if function==0:
+      break
+    if function>4:
+      print("press 1 ,2 ,3, or 4 as per instructions")
+      continue
+    a= float (input("enter a number:"))
+    b= float (input("enter another number:"))
 
-c=6.6
-d=2.1
-print(c+d)
+    if function==1:
+      print(a+b)
+    elif function==2:
+      print(a-b)
+    elif function==3:
+      print(a*b)
+    elif function==4:
+      print(a/b)
+    else:
+      print("I dont umderstand that")
 
-a= float (input("enter a number:"))
-b= float (input("enter another number:"))
-sum =a+b
-print ("the sum =  %d" %sum)
-    
+
+
