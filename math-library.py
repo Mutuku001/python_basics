9b104c433180ccc8eb211f10a19a365c23764421
ElizabethMbulwa <elizabeth.mutuku21@students.dkut.ac.ke>
2023-03-27 00:46:00 -0700

    math-library.py

---
+++ math-library.py
@@ -0,0 +1,15 @@
+import math
+print(math.sqrt(25))
+
+import math as m
+print(m.sin(90))
+print(m.cos(25))
+print(m.tan(10))
+
+import math
+radius = float(input("Enter the radius of the cylinder: "))
+height = float(input("Enter the height of the cylinder: "))
+volume = math.pi * radius**2 * height
+print(f"The volume of the cylinder is: {volume:.2f}")
+
+
